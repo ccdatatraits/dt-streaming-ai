3fc951e706e45458cf8c2da34fdb75533f9a09f9
Sohail M. Khan <sohail.munir@gmail.com>
2023-11-02 23:23:23 +0400

    Forms

--- crates/axum-server/src/main.rs
+++ crates/axum-server/src/main.rs
@@ -2,8 +2,15 @@ mod config;
 mod errors;
 
 use crate::errors::CustomError;
-use axum::{extract::Extension, response::Html, routing::get, Router};
+// 👇 update axum imports
+use axum::{
+    extract::Extension, http::StatusCode, response::Html, response::IntoResponse,
+    response::Redirect, response::Response, routing::get, routing::post, Form, Router,
+};
+use serde::Deserialize;
 use std::net::SocketAddr;
+// 👇 new import
+use validator::Validate;
 
 #[tokio::main]
 async fn main() {
@@ -14,6 +21,7 @@ async fn main() {
     // build our application with a route
     let app = Router::new()
         .route("/", get(users))
+        .route("/sign_up", post(accept_form))
         .layer(Extension(config))
         .layer(Extension(pool.clone()));
 
@@ -33,4 +41,33 @@ async fn users(Extension(pool): Extension<db::Pool>) -> Result<Html<String>, Cus
 
     // We now return HTML
     Ok(Html(ui_components::users::users(users)))
+}
+
+#[derive(Deserialize, Validate)]
+struct SignUp {
+    #[validate(email)] // 👈 add validate annotation
+    email: String,
+}
+
+async fn accept_form(
+    Extension(pool): Extension<db::Pool>,
+    Form(form): Form<SignUp>,
+    // 👇 change `Redirect` to `Response`
+) -> Result<Response, CustomError> {
+    // 👇 add our error handling
+    if form.validate().is_err() {
+        return Ok((StatusCode::BAD_REQUEST, "Bad request").into_response());
+    }
+
+    let client = pool.get().await?;
+
+    let email = form.email;
+    // TODO - accept a password and hash it
+    let hashed_password = String::from("aaaa");
+    let _ = db::queries::users::create_user()
+        .bind(&client, &email.as_str(), &hashed_password.as_str())
+        .await?;
+
+    // 303 redirect to users list
+    Ok(Redirect::to("/").into_response()) // 👈 add `.into_response()`
 }
--- crates/ui-components/src/users.rs
+++ crates/ui-components/src/users.rs
@@ -35,6 +35,15 @@ pub fn users(users: Vec<User>) -> String {
                         ))
                     }
                 }
+
+                // 👇 this is our new form
+                form {
+                    action: "/sign_up",
+                    method: "POST",
+                    label { r#for: "user_email", "Email:" }
+                    input { id: "user_email", name: "email", r#type: "email", required: "true" }
+                    button { "Submit" }
+                }
             }
         })
     }
